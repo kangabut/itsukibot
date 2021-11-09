18d8bc73e81dad4a0fdf3011484f6cccdec08191
kangabut <82143938+kangabut@users.noreply.github.com>
2021-11-09 10:34:29 +0700

    Create githubinstall.sh

---
+++ githubinstall.sh
@@ -0,0 +1,16 @@
+apt install tty-clock -y
+apt install sox -y
+apt install jq -y
+apt install figlet -y
+apt install wget -y
+apt install ffmpeg libwebp -y
+apt install nodejs -y
+apt install tesseract -y
+wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
+npm install
+npm audit fix
+
+
+tty-clock -s -S -x -c -C 2 -b -B
+clear
+node index
